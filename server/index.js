239850ad20ef2d41e277432c8aab1fddb6d1e74b
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-08 16:43:48 +0300

    cookie parser add

--- server/index.js
+++ server/index.js
@@ -1,23 +1,24 @@
+const cookieParser = require('cookie-parser');
 const ServerStart = require('./src/server');
-const app = require('express')();
+const bodyParser = require('body-parser');
+const express = require('express');
 require('dotenv').config();
+const app = express();
 
 const io = ServerStart(app);
 
-app.get('/', (req, res) => {
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
-  res.end('work');
-
-});
-
-io.use((socket, next) => {
-  
-  console.log(...socket)
-  next();
-  
-});
+app.post('/login', (req, res) => require('./src/api/post/login')(req, res));
+app.post('/regist', (req, res) => require('./src/api/post/registration')(req, res));
+app.post('/lagout', (req, res) => require('./src/api/post/lagout')(req, res));
+app.post('/refresh', (req, res) => require('./src/api/post/refresh')(req, res));
 
 io.on('connection', (socket) => {
+
+  
 
   socket.on("message", msg => {
 
