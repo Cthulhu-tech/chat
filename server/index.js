8b4b29d6a38db78df1b9e0591b64059bc6acd4cb
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-08 18:31:59 +0300

    jwt check socket io add

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const cookieParser = require('cookie-parser');
 const ServerStart = require('./src/server');
 const bodyParser = require('body-parser');
+const {verify}  = require('jsonwebtoken');
 const express = require('express');
 require('dotenv').config();
 const app = express();
@@ -16,10 +17,24 @@ app.post('/regist', (req, res) => require('./src/api/post/registration')(req, re
 app.post('/lagout', (req, res) => require('./src/api/post/lagout')(req, res));
 app.post('/refresh', (req, res) => require('./src/api/post/refresh')(req, res));
 
+io.use((socket, next) => {
+
+  try {
+      
+    verify(socket.handshake.auth.jwt, process.env.ACCESS_TOKEN_SECRET);
+
+    next();
+
+  }catch (e){
+
+    console.log(socket.handshake.auth.jwt)
+
+  }
+
+});
+
 io.on('connection', (socket) => {
-
-  
-
+  console.log('connection')
   socket.on("message", msg => {
 
     console.log(msg);
