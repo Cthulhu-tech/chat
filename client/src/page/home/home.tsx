6c7c49bcc0b4c932b8987c98eb50ecd6e7b383ff
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-09 20:59:50 +0300

    room pageadd

--- client/src/page/home/home.tsx
+++ client/src/page/home/home.tsx
@@ -1,5 +1,17 @@
+import { RoomContainer } from "../../components/room/room";
+import { ReduxStore } from "../../interface/redux";
+import { useSelector } from 'react-redux';
+
 export const Home = () => {
     
-    return <>home</>
+    const store = useSelector((store: ReduxStore) => store.room.room);
+
+    return <main>
+    {store && store.map(room =>{
+
+        return <RoomContainer key={room.id} {...room}/>
+
+    })}
+    </main>
 
 }
