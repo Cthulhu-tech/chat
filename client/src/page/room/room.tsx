9074f4976f7be2d41c377e1dbe31cb8036e84ee5
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-10 21:49:59 +0300

    room logic

--- client/src/page/room/room.tsx
+++ client/src/page/room/room.tsx
@@ -1,26 +1,48 @@
 import { SocketContext } from "../../context/socket";
 import { useContext, useEffect } from 'react';
+import { useParams } from "react-router-dom";
 
 export const Room = () => {
+
+    const {id} = useParams();
 
     const socket = useContext(SocketContext);
 
     useEffect(() => {
 
+        const sendNewMessage = (msg: string) => {
+            
+            console.log(msg);
+    
+        }
+
         if(socket){
 
-            socket.emit('create', 'room')
+            socket.emit('join', id);
+
+            socket.on('new_message_in_room', sendNewMessage);
 
         }
 
-    },[])
+        return () => {
+
+            if(socket){
+
+                socket.removeAllListeners('new_message_in_room');
+                socket.emit('leave', id);
+
+            }
+
+        };
+
+    },[socket]);
 
     const newMessage = () => {
 
-        socket && socket.emit('new_message_in_room', 'new message')
+        socket && socket.emit('room_message', {id, msg:"message"},)
 
     }
 
-    return <div onClick={newMessage}>room</div>
+    return <div onClick={newMessage}>room {id}</div>
 
 }
