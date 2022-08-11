ca3cd5da3ee84a188450eafafaacaa8d28895f4d
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-11 20:05:33 +0300

    update logic

--- client/src/components/socket/socket.tsx
+++ client/src/components/socket/socket.tsx
@@ -1,44 +1,41 @@
 import { newRoomData } from "../../redux/store/all_room";
 import { SocketContext } from "../../context/socket";
 import { ReduxStore } from "../../interface/redux";
-import { io, Socket } from 'socket.io-client';
-import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 export const SocketConnection = () => {
 
     const dispatch = useDispatch();
-    const [socket, setSocket] = useState<Socket>();
 
     const jwt = useSelector((store:ReduxStore) => store.jwt.user.jwt);
+    
+    const _Socket = io("http://localhost:4000",{auth: {jwt},transports: ['websocket'], upgrade: false});
 
     useEffect(() => {
 
-        const socket = io("http://localhost:4000",{auth: {jwt: jwt}, transports: ['websocket'], upgrade: false});
-        setSocket(socket);
+        _Socket.emit('allData_room');
 
-        socket.emit('allData_room');
+        _Socket.on('update_room', () => _Socket.emit('data_room'));
+        _Socket.on('new_message', (msg:string) => console.log(msg));
+        _Socket.on('delete_room', (msg:string) => console.log(msg));
+        _Socket.on('delete_message', (msg:string) => console.log(msg));
+        _Socket.on('update_message', () => _Socket.emit('data_message'));
+        _Socket.on('allData_room', (msg:string) => dispatch(newRoomData(msg)));
 
-        socket.on('allData_room', (msg) => dispatch(newRoomData(msg)));
-
-        socket.on('update_room', () => socket.emit('data_room'));
-        socket.on('update_message', () => socket.emit('data_message'));
-
-        socket.on('new_message', (msg) => console.log(msg));
-        socket.on('delete_room', (msg) => console.log(msg));
-        socket.on('delete_message', (msg) => console.log(msg));
 
         return () => {
 
-            socket.close();
+            _Socket.close();
 
         };
 
-    },[jwt, setSocket]);
+    },[_Socket, jwt]);
 
-    return <SocketContext.Provider value={socket as Socket}>
+    return <SocketContext.Provider value={_Socket}>
         <Outlet/>
     </SocketContext.Provider>
 
