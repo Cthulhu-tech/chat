5ade42debb47f07163050d5adfe3f722fa03d5a4
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-11 20:05:14 +0300

    update logic

--- server/src/socket/socket.js
+++ server/src/socket/socket.js
@@ -8,42 +8,23 @@ class Socket {
 
     }
 
-    connection() {
+    _verify() {
 
-        this.io.use((socket, next) => {
+      this.io.use((socket, next) => {
 
-            try {
-                
-              verify(socket.handshake.auth.jwt, process.env.ACCESS_TOKEN_SECRET);
-          
-              next();
-          
-            }catch (e){
-          
-              console.log(socket.handshake.auth.jwt);
-          
-            }
-          
-        });
-          
-        this.io.on('connection', (socket) => {
-        
-            socket.on('disconnect', () => socket.disconnect(0));
-            socket.on("message", (msg) => require('./message')(msg, socket));
-
-            socket.on('join', (room) => require('./room')(room, socket, this.io));
-            socket.on('leave', (room) => socket.leave(room));
+        try {
             
-            socket.on('allData_room', () => require('./allData_room')(socket, this.io));
-            socket.on('allData_message', () => require('./allData_message')(socket));
-            
-            socket.on('new_room', (msg) => require('./new_room')(msg, socket, this.io));
-            socket.on('new_message', (msg) => require('./new_message')(msg, this.io));
-            
-            socket.on('delete_room', (msg) => require('./delete_room')(msg, socket));
-            socket.on('delete_message', (msg) => require('./delete_message')(msg, socket));
-        
-        });          
+          verify(socket.handshake.auth.jwt, process.env.ACCESS_TOKEN_SECRET);
+      
+          return next();
+      
+        }catch (e){
+      
+          console.log(socket.handshake.auth.jwt);
+      
+        }
+      
+      });
 
     }
 
