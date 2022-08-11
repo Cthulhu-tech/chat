33743bc2cf0d57937aaf95c588f9697b30b80a00
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-12 00:53:21 +0300

    root store update

--- client/src/redux/root.tsx
+++ client/src/redux/root.tsx
@@ -1,11 +1,15 @@
 import { applyMiddleware, combineReducers, createStore } from "redux";
-import { AllRoom } from "./store/all_room";
+import { userMessageStore } from "./store/message";
+import { AllMsgStore } from "./store/allMessage";
+import { AllRoom } from "./store/allRoom";
 import { JWT } from "./store/jwt";
 import thunk from "redux-thunk";
 
 export const rootReducer = combineReducers({
     jwt: JWT,
-    room: AllRoom
+    room: AllRoom,
+    userMsg: userMessageStore,
+    allMsg: AllMsgStore
 });
 
 export type AppDispatch = typeof store.dispatch
