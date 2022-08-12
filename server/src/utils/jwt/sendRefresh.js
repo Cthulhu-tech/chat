cb7668826bc0b5295e172e4e428b0df2940840ec
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-13 01:52:22 +0300

    new logic add

--- server/src/utils/jwt/sendRefresh.js
+++ server/src/utils/jwt/sendRefresh.js
@@ -3,7 +3,7 @@ const sendRefreshToken = (response, token) => {
     response.cookie('refreshtoken', token, {
 
         httpOnly: true,
-        path: '/refresh',
+        path: '/',
         expires: new Date(Date.now() + 432000000)
 
     });
