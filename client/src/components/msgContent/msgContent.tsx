2f9ea628c6e7ebbd498d82d613a8c9b9d73c5b51
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-14 00:49:52 +0300

    msg contend logic update

--- client/src/components/msgContent/msgContent.tsx
+++ client/src/components/msgContent/msgContent.tsx
@@ -1,10 +1,13 @@
 import { joinMsg, updateMsg } from "../../redux/store/allMessage";
 import { Msg, ReduxStore } from "../../interface/redux";
+import { userMessage } from "../../redux/store/message";
 import { SocketContext } from "../../context/socket";
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { Message } from "../input/message";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
+import './msgContent.scss';
 
 export const MsgContent = () => {
 
@@ -12,6 +15,7 @@ export const MsgContent = () => {
     const dispatch = useDispatch();
     const socket = useContext(SocketContext);
     const msg = useSelector((store: ReduxStore) => store.allMsg.msg);
+    const userMsg = useSelector((store: ReduxStore) => store.userMsg.msg);
 
     useEffect(() => {socket.emit('allData_message', id)},[id]);
 
@@ -29,15 +33,37 @@ export const MsgContent = () => {
 
     },[socket]);
 
-    return <div>
-        {msg.map((msg) => {
+    const newMessage = async () => {
 
-            return <div key={msg.id}>
-                <p>user: {msg.user}</p>
-                <span>{msg.msg}</span>
+        if(id){
+            
+            await socket.emit('room_message', {id, msg: userMsg});
+
+            await dispatch(userMessage(''));
+
+        }
+
+    };
+
+    useEffect(() => {}, [id, msg, userMsg])
+
+    return <div className="msg">
+        <div className="container-message">
+            {msg.map((msg) => {
+
+                return <div key={msg.id}>
+                    <p>user: {msg.user}</p>
+                    <span>{msg.msg}</span>
+                </div>
+
+            })}
+        </div>
+        <div className="container-input">
+            <div>
+                <Message/>
+                <button onClick={newMessage}>send message</button>
             </div>
-
-        })}
+        </div>
     </div>
 
 }
