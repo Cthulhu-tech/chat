9dc9cea4ce4c2d4733378d4f8ea86d6d6242be57
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-14 00:50:25 +0300

    room style add

--- client/src/page/home/home.tsx
+++ client/src/page/home/home.tsx
@@ -1,17 +1,30 @@
 import { RoomContainer } from "../../components/room/room";
 import { ReduxStore } from "../../interface/redux";
 import { useSelector } from 'react-redux';
+import { Outlet } from "react-router-dom";
+import './home.scss';
 
 export const Home = () => {
     
     const store = useSelector((store: ReduxStore) => store.room.room);
+    const user = useSelector((store: ReduxStore) => store.jwt.user);
     
-    return <main>
-    {store && store.map(room =>{
+    return <>
+    <aside className="aside shadow-rigth">
+        <div className="container-user">
+            <div className="container-img user-img"></div>
+            <p className="text title description">{user.login}</p>
+        </div>
+        <p className="text title description">All</p>
+        {store && store.map(room =>{
 
-        return <RoomContainer key={room.id} {...room}/>
+            return <RoomContainer key={room.id} {...room}/>
 
-    })}
+        })}
+    </aside>
+    <main className="main">
+        <Outlet/>
     </main>
+    </>
 
 }
