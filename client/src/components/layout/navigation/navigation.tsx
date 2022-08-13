f4f34f96ec6e1e713d8c4c811301bec7eeb5659c
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-14 00:49:16 +0300

    navigation update

--- client/src/components/layout/navigation/navigation.tsx
+++ client/src/components/layout/navigation/navigation.tsx
@@ -1,12 +1,16 @@
 import { ReduxStore } from '../../../interface/redux';
 import { useSelector } from 'react-redux';
 import { Sing } from '../sing/sing';
+import './navigation.scss';
 
 export const Navigation = () => {
 
+    const handlerNavigation = () => {};
+
     const user = useSelector((store: ReduxStore) => store.jwt.user.login);
     
-    return <nav>
+    return <nav className="navigation shadow-bottom">
+        <div className="navigation-handler" onClick={handlerNavigation}/>
         {user && <Sing/>}
     </nav>
 
