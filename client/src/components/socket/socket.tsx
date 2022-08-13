2eaba552d279184fd9a9868234051558d30e14d7
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-13 18:31:06 +0300

    socket logic update

--- client/src/components/socket/socket.tsx
+++ client/src/components/socket/socket.tsx
@@ -1,41 +1,64 @@
+import { socket, SocketContext } from "../../context/socket";
 import { newRoomData } from "../../redux/store/allRoom";
-import { SocketContext } from "../../context/socket";
+import { setEmit } from "../../redux/store/lastEmit";
+import { updateToken } from "../../redux/store/jwt";
 import { ReduxStore } from "../../interface/redux";
 import { useSelector } from "react-redux";
 import { Outlet } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import { io } from "socket.io-client";
 import { useEffect } from "react";
 
 export const SocketConnection = () => {
 
+    const emit = useSelector((store:ReduxStore) => store.emit.emit);
+    const jwt = useSelector((store:ReduxStore) => store.jwt.user.jwt);
+
     const dispatch = useDispatch();
 
-    const jwt = useSelector((store:ReduxStore) => store.jwt.user.jwt);
+    const update_jwt = (data:string[]) => {
+
+        dispatch(setEmit(data));
+
+        dispatch(updateToken({user: {
+
+            login: null,
+            jwt: null,
     
-    const _Socket = io("http://localhost:4000",{auth: {jwt},transports: ['websocket'], upgrade: false});
+        }}));
+
+    }
 
     useEffect(() => {
 
-        _Socket.emit('allData_room');
+        socket.auth = {jwt};
+        socket.connect();
 
-        _Socket.on('update_room', () => _Socket.emit('data_room'));
-        _Socket.on('new_message', (msg:string) => console.log(msg));
-        _Socket.on('delete_room', (msg:string) => console.log(msg));
-        _Socket.on('delete_message', (msg:string) => console.log(msg));
-        _Socket.on('update_message', () => _Socket.emit('data_message'));
-        _Socket.on('allData_room', (msg:string) => dispatch(newRoomData(msg)));
+        if(emit){
+
+            socket.emit(emit[0], emit[1]);
+
+        }
+
+        socket.emit('allData_room');
+
+        socket.on('update_jwt', update_jwt);
+        socket.on('new_message', (msg:string) => console.log(msg));
+        socket.on('delete_room', (msg:string) => console.log(msg));
+        socket.on('delete_message', (msg:string) => console.log(msg));
+        socket.on('update_room', () => socket.emit('data_room'));
+        socket.on('update_message', () => socket.emit('data_message'));
+        socket.on('allData_room', (msg:string) => dispatch(newRoomData(msg)));
 
 
         return () => {
 
-            _Socket.close();
+            socket.close();
 
         };
 
-    },[_Socket, jwt]);
+    },[jwt]);
 
-    return <SocketContext.Provider value={_Socket}>
+    return <SocketContext.Provider value={socket}>
         <Outlet/>
     </SocketContext.Provider>
 
