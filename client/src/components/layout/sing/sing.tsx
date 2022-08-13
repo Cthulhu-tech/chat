93f52f05ba7144524bdff65eb0ce1fe3ec44079e
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-14 00:49:32 +0300

    sing update

--- client/src/components/layout/sing/sing.tsx
+++ client/src/components/layout/sing/sing.tsx
@@ -1,14 +1,11 @@
 import { updateToken } from '../../../redux/store/jwt';
-import { ReduxStore } from '../../../interface/redux';
 import { useFetch } from '../../../hook/useFetch';
-import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 
 export const Sing = () => {
 
     const dispatch = useDispatch();
-    const user = useSelector((store: ReduxStore) => store.jwt.user.login);
 
     const {load, data, error, FetchData} = useFetch('POST');
 
@@ -29,9 +26,8 @@ export const Sing = () => {
 
     }, [load]);
 
-    return <>
-        <p>{"user: " + user}</p>
-        <p onClick={Lagout}>lagout</p>
-    </>
+    return <div>
+        <p className="text description title" onClick={Lagout}>lagout</p>
+    </div>
 
 }
