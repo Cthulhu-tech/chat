68bb6fe6ada1d882ca6bb85f7560bd2f45168078
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-13 18:34:13 +0300

    refresh logic update

--- server/src/api/post/refresh.js
+++ server/src/api/post/refresh.js
@@ -29,14 +29,14 @@ const refresh = async (request, response) => {
       const connection = await connectDB();
   
       const jwt = await connection.query('SELECT * FROM jwt WHERE user_id = ? and jwt = ?', [payload.userId, token]);
-      
+
       const user = await connection.query('SELECT * FROM user WHERE id = ?', [payload.userId]);
 
       if (!user[0][0]) return response.send({ accesstoken: null });
 
       if (jwt[0].length === 0 || jwt[0][0].jwt !== token)
         return response.status(401).json({ accesstoken: null });
-  
+
       const accesstoken = await createAccessToken(user[0][0].id, "1m");
 
       const refreshtoken = await createRefreshToken(user[0][0].id, "7d");
@@ -46,7 +46,7 @@ const refresh = async (request, response) => {
       await connection.query('INSERT INTO jwt (user_id, jwt) VALUES (?, ?)', [payload.userId, refreshtoken]);
 
       const user_info = await connection.query('SELECT * FROM user WHERE id = ?', [payload.userId]);
-  
+
       sendRefreshToken(response, refreshtoken);
       
       connection.end();
