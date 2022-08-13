e5ef9255f79ed47b4c2332cd24b3f9cf4b580d35
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-14 00:50:41 +0300

    update

--- client/src/page/room/room.tsx
+++ client/src/page/room/room.tsx
@@ -1,44 +1,43 @@
 import { MsgContent } from "../../components/msgContent/msgContent";
-import { Message } from "../../components/input/message";
+import { ErrorComponent } from "../../components/error/error";
+import { useContext, useEffect, useState } from 'react';
 import { SocketContext } from "../../context/socket";
 import { ReduxStore } from "../../interface/redux";
-import { useContext, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
+import './room.scss';
 
 export const Room = () => {
+
+    const [found, setFound] = useState(false);
 
     const {id} = useParams();
     const socket = useContext(SocketContext);
     const msg = useSelector((store: ReduxStore) => store.userMsg.msg);
     const jwt = useSelector((store:ReduxStore) => store.jwt.user.jwt);
 
-    const newMessage = () => {
-        
-            console.log(socket)
-        socket.emit('room_message', {id, msg});}
+    const Redirect = () => setFound(true);
 
     useEffect(() => {},[msg]);
 
     useEffect(() => {
 
         socket.emit('join', id);
+        socket.on('room_not_found', Redirect);
 
         return () => {
 
+            socket.off('room_not_found');
             socket.emit('leave', id);
 
         };
 
-    },[socket, jwt]);
+    },[socket, jwt, id]);
 
+    if(found) return <ErrorComponent error={'Такой комнаты не существует'}/>
 
-    return <section>
+    return <section className="room-chat shadow-rigth">
         <MsgContent/>
-        <div>
-            <Message/>
-            <button onClick={newMessage}>send message</button>
-        </div>
     </section>
 
 }
