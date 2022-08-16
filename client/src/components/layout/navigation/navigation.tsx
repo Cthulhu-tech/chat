4f8469094a8f8b342a91ef0ca7ba98a7b2f12634
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-16 03:01:14 +0300

    msg add

--- client/src/components/layout/navigation/navigation.tsx
+++ client/src/components/layout/navigation/navigation.tsx
@@ -1,16 +1,23 @@
+import { toggleNavigation } from '../../../redux/store/navigation';
 import { ReduxStore } from '../../../interface/redux';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { Sing } from '../sing/sing';
 import './navigation.scss';
 
 export const Navigation = () => {
 
-    const handlerNavigation = () => {};
+    const dispatch = useDispatch();
+    const active = useSelector((store: ReduxStore) => store.navigation.open);
+
+    const handlerNavigation = () => dispatch(toggleNavigation(!active));
 
     const user = useSelector((store: ReduxStore) => store.jwt.user.login);
     
     return <nav className="navigation shadow-bottom">
-        <div className="navigation-handler" onClick={handlerNavigation}/>
+        <div className="navigation-handler-container" onClick={handlerNavigation}>
+            <div className="navigation-handler"/>
+        </div>
         {user && <Sing/>}
     </nav>
 
