4cfcb60cd63ec2a583ad0758f89e13c2f97f8278
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-16 03:01:00 +0300

    auto scroll add

--- client/src/components/msgContent/msgContent.tsx
+++ client/src/components/msgContent/msgContent.tsx
@@ -2,20 +2,24 @@ import { joinMsg, updateMsg } from "../../redux/store/allMessage";
 import { Msg, ReduxStore } from "../../interface/redux";
 import { userMessage } from "../../redux/store/message";
 import { SocketContext } from "../../context/socket";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { Message } from "../input/message";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 import './msgContent.scss';
 
+import send from '../../assets/message/send.png';
+
 export const MsgContent = () => {
 
     const {id} = useParams();
     const dispatch = useDispatch();
     const socket = useContext(SocketContext);
+    const refMsg = useRef<HTMLDivElement>(null);
     const msg = useSelector((store: ReduxStore) => store.allMsg.msg);
     const userMsg = useSelector((store: ReduxStore) => store.userMsg.msg);
+    const userName = useSelector((store: ReduxStore) => store.jwt.user.login);
 
     useEffect(() => {socket.emit('allData_message', id)},[id]);
 
@@ -45,23 +49,32 @@ export const MsgContent = () => {
 
     };
 
-    useEffect(() => {}, [id, msg, userMsg])
+    const scrollMessage = () => refMsg.current && refMsg.current.scrollIntoView({ behavior: "smooth" });
+
+    useEffect(() => {}, [id, msg, userMsg]);
+
+    useEffect(() => {scrollMessage();}, [msg]);
 
     return <div className="msg">
         <div className="container-message">
             {msg.map((msg) => {
 
-                return <div key={msg.id}>
-                    <p>user: {msg.user}</p>
-                    <span>{msg.msg}</span>
+                return <div key={msg.id} className="message">
+                    <div className={msg.user === userName ? "container-message-data shadow-bottom user" : "container-message-data shadow-bottom companion"}>
+                        <p className="text">{msg.msg ? msg.msg : 'Оповещение'}</p>
+                        {msg.user !== userName && <p className="text msg-user">{msg.user}</p>}
+                    </div>
                 </div>
 
             })}
+            <div ref={refMsg}/>
         </div>
         <div className="container-input">
-            <div>
+            <div className="sub-container-input">
                 <Message/>
-                <button onClick={newMessage}>send message</button>
+                <button className="btn btn-message" onClick={newMessage}>
+                    <img src={send} alt="send" className="img-send" />
+                </button>
             </div>
         </div>
     </div>
