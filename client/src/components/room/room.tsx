2bde093b7636017db6d28b29b55c8fa9a64a30d8
Cthulhu-tech <mentsoff.daniar@yandex.ru>
2022-08-16 03:08:49 +0300

    room update

--- client/src/components/room/room.tsx
+++ client/src/components/room/room.tsx
@@ -1,12 +1,15 @@
-import { Room } from "../../interface/redux";
+import { ReduxStore, Room } from "../../interface/redux";
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 import './room.scss';
 
 export const RoomContainer = (room: Room) => {
 
+    const active = useSelector((store: ReduxStore) => store.navigation.open);
+
     return <NavLink className="description container-room text" to={`/room/${room.id}`}>
         <div className="container-img"></div>
-        {room.name}
+        {active && room.name}
     </NavLink>
 
 
